a8badd13e803673c2d9690d47db4bac6bee22b07
wobsoriano <sorianorobertc@gmail.com>
2022-11-16 10:53:37 -0800

    remove .vercel output

--- .vercel/output/functions/__nitro.func/chunks/hello.mjs
+++
@@ -1,6 +0,0 @@
-import { eventHandler } from 'h3';
-
-const hello = eventHandler(() => ({ text: "hello from nuxt folder /api/hello.ts" }));
-
-export { hello as default };
-//# sourceMappingURL=hello.mjs.map
--- .vercel/output/static/_nuxt/composables.95ee6660.js
+++
@@ -1,1 +0,0 @@
-import{k as r}from"./entry.ac90d970.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
