a3b5ec340f64804e72a89d343fcc85cd9d9e67cd
wobsoriano <sorianorobertc@gmail.com>
2022-11-16 10:56:05 -0800

    add repo link

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <iframe src="/api/hello" frameborder="0"></iframe> <br />
     <iframe src="/api/h3" frameborder="0"></iframe> <br />
     <iframe src="/api/express" frameborder="0"></iframe> <br />
-    <iframe src="/api/fastify" frameborder="0"></iframe>
+    <iframe src="/api/fastify" frameborder="0"></iframe> <br />
+    <a href="https://github.com/wobsoriano/nuxt-subsystem-demo">Repo</a>
   </div>
 </template>
